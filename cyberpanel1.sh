7441df0ed5965d59ccda71e7dc3b2dd99c29ea02
Ian Small <121866422+iansmall26@users.noreply.github.com>
2024-02-09 19:08:35 +0300

    Update cyberpanel1.sh

--- cyberpanel1.sh
+++ cyberpanel1.sh
@@ -4,5 +4,6 @@ OUTPUT=$(cat /etc/*release)
 if echo $OUTPUT | grep -q "Ubuntu 20.04" ; then
 apt install -y -qq wget curl
                 SERVER_OS="Ubuntu"
+fi
 curl --silent -o cyberpanel.sh "https://cyberpanel.sh/?dl&$SERVER_OS" 2>/dev/null
 nano cyberpanel.sh
